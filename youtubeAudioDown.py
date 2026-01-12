a92cca2e64bcf54e36020e99272df9b78df081d9
tripthelight <bye_ok@naver.com>
2026-01-12 17:52:20 +0900

    [260112] - card num bit operat working ...

---
+++ youtubeAudioDown.py
@@ -0,0 +1,3 @@
+# https://www.clien.net/service/board/lecture/18065437
+
+# yt-dlp -P "C:\Users\Administrator\Downloads\dw" -x "https://youtu.be/1W2n45jU9ck" --audio-format "mp3" --audio-quality 256k --sponsorblock-remove all
---
+++ youtubeVideoDown.py
@@ -0,0 +1,43 @@
+# 유튜브 화질이 1080p 인 경우 video만 추출됨
+# url : https://4ourfuture.tistory.com/32
+# 실행파일 생성 : pyinstaller --onefile --windowed youtubeVideoDown.py
+
+from pytubefix import YouTube
+import tkinter as tk
+from tkinter import messagebox
+
+def download_youtube_video():
+  video_url = url_entry.get()
+  download_path = path_entry.get()
+
+  if not video_url or not download_path:
+    messagebox.showerror("오류", "URL과 다운로드 경로를 입력하세요.")
+    return
+
+  try:
+    yt = YouTube(video_url)
+    video_stream = yt.streams.filter(res='1080p', file_extension='mp4').first()
+    video_stream.download(download_path)
+    messagebox.showinfo("완료", f"{video_stream.title} 다운로드 완료!")
+
+  except Exception as e:
+    messagebox.showerror("오류 발생", str(e))
+
+root = tk.Tk()
+root.title("유튜브 영상 다운로드")
+
+tk.Label(root, text="유튜브 영상 URL:").pack()
+url_entry = tk.Entry(root, width=50)
+url_entry.pack()
+
+tk.Label(root, text="다운로드 경로:").pack()
+path_entry = tk.Entry(root, width=50)
+path_entry.pack()
+
+download_button = tk.Button(root, text="다운로드", command=download_youtube_video)
+download_button.pack()
+
+root.mainloop()
+
+#############################################################
+
