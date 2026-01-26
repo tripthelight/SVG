4f9aa9c15900de972705e74476b21aaa8278e281
tripthelight <bye_ok@naver.com>
2026-01-26 20:28:12 +0900

    [260126] - promise try catch error handling

--- errorHandling/fn2.js
+++ errorHandling/fn2.js
@@ -2,7 +2,7 @@ import fn3 from "./fn3.js";
 
 export default () => {
   try {
-    fn3()
+    /* fn3()
       .then((result) => {
         //
       })
@@ -10,11 +10,81 @@ export default () => {
         console.log("promise 에서 잡음");
         console.log("errCase : ", error.errCase);
         console.log("message : ", error.message);
-      });
+      }); */
 
-    // if (ERR) {
-    //   throw { errCase: "comn", message: "fn2 error test" };
-    // };
+
+    /* const ERR = true;
+    const PROMISE1 = new Promise((resolve, reject) => {
+      resolve(ERR);
+    });
+    PROMISE1
+      .then((_data) => {
+        const PROMISE2 = new Promise((resolve, reject) => {
+          fn3()
+            .then(() => {
+              resolve(_data);
+            })
+            .catch(error => {
+              reject(error);
+              // console.log("fn3 에서 잡음");
+              // console.log("errCase : ", error.errCase);
+              // console.log("message : ", error.message);
+            })
+        });
+        PROMISE2
+          .then(() => {
+            const PROMISE3 = new Promise((resolve, reject) => {
+              resolve();
+            });
+            PROMISE3
+              .then(() => {
+                //
+              })
+              .catch(error => {
+                console.log("PROMISE3 에서 잡음");
+                console.log("errCase : ", error.errCase);
+                console.log("message : ", error.message);
+              })
+          })
+          .catch(error => {
+            console.log("PROMISE2 에서 잡음");
+            console.log("errCase : ", error.errCase);
+            console.log("message : ", error.message);
+          })
+      })
+      .catch((error) => {
+        console.log("PROMISE1 에서 잡음");
+        console.log("errCase : ", error.errCase);
+        console.log("message : ", error.message);
+      }); */
+
+    const ERR = true;
+    const PROMISE1 = new Promise((resolve, reject) => {
+      try {
+        if (ERR) {
+          throw { errCase: "trycatch", message: "Primise try catch TEST" }
+        };
+        resolve(ERR)
+      } catch (error) {
+        reject(error);
+      }
+    });
+    PROMISE1
+      .then((_data) => {
+        try {
+          if (ERR) {
+            throw { errCase: "then", message: "Primise then TEST" }
+          }
+        } catch (error) {
+          throw error
+        }
+      })
+      .catch((error) => {
+        console.log("PROMISE1 에서 잡음");
+        console.log("errCase : ", error.errCase);
+        console.log("message : ", error.message);
+      })
+
   } catch (error) {
     throw error;
   }
