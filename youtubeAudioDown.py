8278e4172c3639790a7d1b38847a507eb5d1a273
tripthelight <bye_ok@naver.com>
2026-08-16 21:30:07 +0900

    [260816]

--- youtubeAudioDown.py
+++ youtubeAudioDown.py
@@ -4,6 +4,12 @@
 # pip install ffmpeg
 
 # yt-dlp -P "Y:/dw/숙명여대" -x "https://youtu.be/1W2n45jU9ck" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
+# yt-dlp -P "D:/05_YOUTUBE/추적60분" -x "https://youtu.be/AxygASqC0T8" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
+# yt-dlp -P "" -x "" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
+# yt-dlp -P "" -x "" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
+# yt-dlp -P "" -x "" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
+# yt-dlp -P "" -x "" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
+# yt-dlp -P "" -x "" --audio-format "mp3" --audio-quality 128k --sponsorblock-remove all
 
 
 
--- youtubeVdoAdoMerge.py
+++ youtubeVdoAdoMerge.py
@@ -1,3 +1,14 @@
 # https://werw.tistory.com/65
 
 # ffmpeg -i 질풍가도_3.mp4 -i 질풍가도_3.mp3 질풍가도_3_a.mp4
+# ffmpeg -i "성공팔이에_혹하는_청년들.mp4" -i "성공팔이에_혹하는_청년들.mp3" "성공팔이에_혹하는_청년들_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
+# ffmpeg -i ".mp4" -i ".mp3" "_merge.mp4"
